0478537df76f5a7f2e1fc777f2b6278ce708a45f
Emiliano Ferreyra <emiliano.ferreyra@mediamonks.com>
2019-12-23 09:07:10 -0300

    more arrays

--- arrays.ts
+++ arrays.ts
@@ -15,4 +15,12 @@ const myCar = carMakers.pop();
 // Prevent incompatible values
 carMakers.push(100);
 
-/
+// Help with 'map'
+carMakers.map((car: string): string => {
+  return car.toUpperCase();
+});
+
+// Flexible types
+const importantDates: (Date | string)[] = [new Date()];
+importantDates.push('2030-10-10');
+importantDates.push(new Date());
